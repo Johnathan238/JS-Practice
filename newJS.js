9614bb2b018b05698a92aac7c7f4cc2093c53838
Johnathan <jonathanumana86@gmail.com>
2021-05-08 23:42:14 -0400

    Having fun with alert

--- newJS.js
+++ newJS.js
@@ -1,3 +1,10 @@
 // console.log("WAKE UP, ITS THE FIRST OF THE MONTH, I BRUSH MY TEETH & COUNT UP")
-alert("WHERE TF IS MARIO JUDAH")
-alert("I MISS THE RAGE")
+// alert("WHERE TF IS MARIO JUDAH")
+// alert("I MISS THE RAGE")
+
+const mJ = 'I MISS THE RAGE' 
+if (mJ === 'I miss the rage') {
+    alert ('YESSSSS')
+} else {
+    alert('WHERE TF IS MARIO JUDAH')
+}
