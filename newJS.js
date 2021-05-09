6115f5ea7888f1352e8701adc4100e85594a3e3f
Johnathan <jonathanumana86@gmail.com>
2021-05-09 00:01:38 -0400

    Got it to log on the console

--- newJS.js
+++ newJS.js
@@ -1,10 +1,22 @@
-// console.log("WAKE UP, ITS THE FIRST OF THE MONTH, I BRUSH MY TEETH & COUNT UP")
+console.log("WAKE UP, ITS THE FIRST OF THE MONTH, I BRUSH MY TEETH & COUNT UP")
 // alert("WHERE TF IS MARIO JUDAH")
 // alert("I MISS THE RAGE")
 
-const mJ = 'I MISS THE RAGE' 
-if (mJ === 'I miss the rage') {
-    alert ('YESSSSS')
-} else {
-    alert('WHERE TF IS MARIO JUDAH')
+// const mJ = 'I MISS THE RAGE' 
+// if (mJ === 'I miss the rage') {
+//     alert ('YESSSSS')
+// } else {
+//     alert('WHERE TF IS MARIO JUDAH')
+// }
+
+// 'mario judah' = is a value
+
+const age = 20;
+
+function foo() {
+    const age = 29;
+    console.log(age);
 }
+
+foo()
+console.log(age);
