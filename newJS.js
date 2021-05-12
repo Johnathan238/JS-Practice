a5e30f455426ec27360d989fa178d53b3e94350b
Johnathan238 <jonathanumana86@gmail.com>
2021-05-12 00:31:27 -0400

    Trying out basic operators

--- newJS.js
+++ newJS.js
@@ -1,11 +1,11 @@
-console.log("WAKE UP, ITS THE FIRST OF THE MONTH, I BRUSH MY TEETH & COUNT UP")
+// console.log("WAKE UP, ITS THE FIRST OF THE MONTH, I BRUSH MY TEETH & COUNT UP")
 
-// const mJ = 'I MISS THE RAGE'
-// if (mJ === 'I miss the rage') {
-//     alert ('YESSSSS')
-// } else {
-//     alert('WHERE TF IS MARIO JUDAH')
-// }
+const mJ = 'I MISS THE RAGE'
+if (mJ === 'I miss the rage') {
+    alert ('YESSSSS')
+} else {
+    alert('WHERE TF IS MARIO JUDAH')
+};
 
 // 'mario judah' = is a value
 
@@ -19,13 +19,16 @@ console.log("WAKE UP, ITS THE FIRST OF THE MONTH, I BRUSH MY TEETH & COUNT UP")
 // foo()
 // console.log(age);
 
-const firstName = "Mario Judah" // Declaring a variable
-console.log(typeof firstName);
+// const firstName = "Mario Judah" // Declaring a variable
+// console.log(typeof firstName);
 
-const mJ = "I MISS THE RAGE"
-console.log(mJ);
-// alert("WHERE TF IS MARIO JUDAH")
-// alert("I MISS THE RAGE")
+// const mJ = "I MISS THE RAGE"
+// console.log(mJ);
+// // alert("WHERE TF IS MARIO JUDAH")
+// // alert("I MISS THE RAGE")
 
-rockStar = "I MISS THE RAGE!"
-console.log(rockStar);
+// rockStar = "I MISS THE RAGE!"
+// console.log(rockStar);
+
+const age = 2045 - 2000
+console.log(age);
