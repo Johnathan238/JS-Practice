60610801ccf243a2a2122a4420cf79b78817b947
Johnathan238 <jonathanumana86@gmail.com>
2021-05-23 00:39:41 -0400

    I understand the truthy and falsy now

--- newJS.js
+++ newJS.js
@@ -146,3 +146,12 @@ console.log(Boolean('Mario Judah'))
 console.log(Boolean({}))
 console.log(Boolean(''))
 
+const money = 0;
+// if (money) {
+//   console.log("Don't spend it 💲")
+// } else {
+//   console.log("You need to get cheese 💲")
+// }
+const newMoney = money ? console.log("Don't spend it 💲") : console.log("You need to get cheese 💲")
+
+
