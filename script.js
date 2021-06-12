510b8be33d9b0e1b676bd158dddd01984d18079c
Johnathan <jonathanumana86@gmail.com>
2021-06-11 20:26:56 -0400

    Got the funxction to work, now to practice on my own

--- script.js
+++ script.js
@@ -18,5 +18,9 @@ function marioJudah() {
 marioJudah()
 
 function fruitProcessor (apples, oranges) {
-    
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges. `
+
+    return juice
 }
+alert(fruitProcessor(5, 3))
