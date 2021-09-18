9e97bd60c097efcbd6e216bb54004e4fd4e989ea
Johnathan <jonathanumana86@gmail.com>
2021-09-18 16:50:14 -0400

    While loop is good, I perfer a for loop

--- script.js
+++ script.js
@@ -550,10 +550,11 @@ marioJudah()
 //   rep++;
 // }
 
-let dice = Math.trunc(Math.random() * 6)
+let dice = Math.trunc(Math.random() * 6) + 1
 
 
 while (dice !== 6){
   console.log(`You rolled a ${dice}`);
-  dice = Math.trunc(Math.random() * 6)
+  dice = Math.trunc(Math.random() * 6) + 1
+  if (dice === 6) console.log(`Loop is about to end at ${dice}`);
 } 
