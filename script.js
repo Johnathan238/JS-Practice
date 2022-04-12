e40bb417aa21b18e27ce82d77ec8ffd9e0b3a1b3
Johnathan <jonathanumana86@gmail.com>
2022-04-12 13:24:39 -0400

    created one player array for each team

--- script.js
+++ script.js
@@ -1310,35 +1310,126 @@
 // console.log('====================================');
 
 //** Logical Assigment Operator**/
-const idk1 = {
-  name: 'Podcast-Ayooo',
-  guest: 0
-}
+// const idk1 = {
+//   name: 'Podcast-Ayooo',
+//   guest: 0
+// }
 
-const idk2 = {
-  name: 'Podcast-WTF',
-  Owner: 'Freshyy'
-}
+// const idk2 = {
+//   name: 'Podcast-WTF',
+//   Owner: 'Freshyy'
+// }
 
-// OR assignment operator
-// idk2.guest = idk1.guest || 4
-// console.log("🚀 ~ file: script.js ~ line 1324 ~ idk2.guest", idk2.guest)
+// // OR assignment operator
+// // idk2.guest = idk1.guest || 4
+// // console.log("🚀 ~ file: script.js ~ line 1324 ~ idk2.guest", idk2.guest)
 
-// idk1.guest ||= 4
-// console.log("🚀 ~ file: script.js ~ line 1328 ~ idk1.guest", idk1.guest)
-// idk2.guest ||= 4
-// console.log("🚀 ~ file: script.js ~ line 1330 ~ idk2.guest", idk2.guest)
+// // idk1.guest ||= 4
+// // console.log("🚀 ~ file: script.js ~ line 1328 ~ idk1.guest", idk1.guest)
+// // idk2.guest ||= 4
+// // console.log("🚀 ~ file: script.js ~ line 1330 ~ idk2.guest", idk2.guest)
 
-// nullish assignment operator (null or undefined)
-// idk1.guest ??= 4
-// console.log("🚀 ~ file: script.js ~ line 1328 ~ idk1.guest", idk1.guest)
-// idk2.guest ??= 4
-// console.log("🚀 ~ file: script.js ~ line 1330 ~ idk2.guest", idk2.guest)
+// // nullish assignment operator (null or undefined)
+// // idk1.guest ??= 4
+// // console.log("🚀 ~ file: script.js ~ line 1328 ~ idk1.guest", idk1.guest)
+// // idk2.guest ??= 4
+// // console.log("🚀 ~ file: script.js ~ line 1330 ~ idk2.guest", idk2.guest)
 
-// && OPERATOR
-// idk2.Owner = idk2.Owner && '<<ANONYMOUS>>'
-idk1.Owner &&= '<<ANONYMOUS>>'
-console.log("🚀 ~ file: script.js ~ line 1341 ~ idk1.Owner", idk1.Owner)
-idk2.Owner &&= '<<ANONYMOUS>>'
-console.log("🚀 ~ file: script.js ~ line 1340 ~ idk2.Owner", idk2.Owner)
+// // && OPERATOR
+// // idk2.Owner = idk2.Owner && '<<ANONYMOUS>>'
+// idk1.Owner &&= '<<ANONYMOUS>>'
+// console.log("🚀 ~ file: script.js ~ line 1341 ~ idk1.Owner", idk1.Owner)
+// idk2.Owner &&= '<<ANONYMOUS>>'
+// console.log("🚀 ~ file: script.js ~ line 1340 ~ idk2.Owner", idk2.Owner)
 
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+We're building a football betting app (soccer for my American friends 😅)!
+Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
+1. Create one player array for each team (variables 'players1' and 'players2')
+2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored */
+
+// 1. -> //
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+const [players1, players2] = game.players
+console.log("🚀 ~ file: script.js ~ line 1404 ~ players1, players2", players1, players2)
+
+
+
+//** Solution **//
+// 1.
+// const [players1, players2] = game.players;
+// console.log(players1, players2);
+// // 2.
+// const [gk, ...fieldPlayers] = players1;
+// console.log(gk, fieldPlayers);
+// // 3.
+// const allPlayers = [...players1, ...players2];
+// console.log(allPlayers);
+// // 4.
+// const players1Final = [...players1, 'Thiago', 'Coutinho', 'Periscic'];
+// // 5.
+// const {
+//   odds: { team1, x: draw, team2 },
+// } = game;
+// console.log(team1, draw, team2);
+// // 6.
+// const printGoals = function (...players) {
+//   console.log(players);
+//   console.log(`${players.length} goals were scored`);
+// };
+// // printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+// // printGoals('Davies', 'Muller');
+// printGoals(...game.scored);
+// // 7.
+// team1 < team2 && console.log('Team 1 is more likely to win');
+// team1 > team2 && console.log('Team 2 is more likely to win');
