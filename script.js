7ebab4569e8e623ac86f8b99b087e4982688e521
Johnathan238 <jonathanumana86@gmail.com>
2022-05-29 17:50:40 -0500

    Challenge number 3 for dtat structures

--- script.js
+++ script.js
@@ -2002,3 +2002,32 @@ GOOD LUCK 😀
 // to map key to values
 //  Use when you need keys
 //  that are not strings
+
+/*1. Create an array'events'of the different game
+events that happened(no duplicates)
+2. After the game has finished,is was found that the
+yellow card from minute 64 was fair.So remove this
+event from the game events log.
+3. Print the following string to the console:"An
+event happened,on average,every9minutes"(keep in
+mind thatagame has 90 minutes)
+4. Loop over the events and log them to the console,
+marking whether it's in the first half or second half
+(after 45 min)of the game,like this:
+       [FIRST HALF]17:GOAL
+GOOD LUCK
+*/
+
+const gameEvents = new Map([
+[17, 'GOAL'],
+[36,'Substitution'],
+[47,'GOAL'],
+[61,'Substitution'],
+[64,'Yellow card'],
+[69,'Red card'],
+[70,'Substitution'],
+[72,'Substitution'],
+[76,'GOAL'],
+[80,'GOAL'],
+[92,'Yellow card'],
+]);
